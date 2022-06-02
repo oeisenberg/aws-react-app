1854a65bbcbaa2c740670f6091e48a790211e285
Oliver <oliver.eisenberg88@gmail.com>
2022-06-02 14:49:16 +0100

    Simplified and Reformatted

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from "./logo.svg";
 import "@aws-amplify/ui-react/styles.css";
-import {
-  withAuthenticator,
-  Button,
-  Heading,
-  Image,
-  View,
-  Card,
-} from "@aws-amplify/ui-react";
+import CssBaseline from "@mui/material/CssBaseline";
+import * as React from "react";
+import ReactDOM from "react-dom";
+import { withAuthenticator, Heading, Button, View, Card } from "@aws-amplify/ui-react";
+import Container from "@mui/material/Container";
 
 function App({ signOut }) {
   return (
-    <View className="App">
-      <Card>
-        <Image src={logo} className="App-logo" alt="logo" />
-        <Heading level={1}>We now have Auth!</Heading>
-      </Card>
-      <Button onClick={signOut}>Sign Out</Button>
-    </View>
+    <React.Fragment>
+      <CssBaseline />
+      <View className="App">
+        <Container maxWidth="sm">
+          <Heading level={1}>Website</Heading>
+          <Button onClick={signOut}>Sign Out</Button>
+        </Container>
+      </View>
+    </React.Fragment>
   );
 }
 
